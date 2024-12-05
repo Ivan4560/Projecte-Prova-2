4c61b008bcbd39af729ca9778d7524c97978fc79
Ivan4560 <ivan.garrido.7e8@itb.cat>
2024-12-05 13:31:43 +0100

    Create Exercici6.cs

---
+++ Exercici6.cs
@@ -0,0 +1,7 @@
+/* valors limit:
+els valor limits tindriem com fora de rang tot el que serian acents i caracters especial tambe fora de rang
+tidriem tot els valors introduits que no superin els 5 caracters o que tinguin mes de 15 caracters i que tinguin un numero al
+principi o final
+
+
+*/
